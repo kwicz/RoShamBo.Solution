176aafaa52c6f2442cfa01d30d52974062b04f72
K. Wicz and Rachel Schieferstein <k.+rachel@>
2020-03-09 11:49:08 -0700

    add file and form for Nounadelphia MadLib

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -7,11 +7,13 @@ namespace MadLibs.Controllers
   {
     
     [Route("/")]
-    public ActionResult Form() { return View(); }
+    public ActionResult Select() { return View(); }
 
+    [Route("form1")]
+    public ActionResult form1() { return View(); }
 
-    [Route("/result")]
-    public ActionResult Result(string noun1, string noun2, string adjective1, string adjective2, string verb1,string verb2, string adverb)
+    [Route("/result1")]
+    public ActionResult Result1(string noun1, string noun2, string adjective1, string adjective2, string verb1,string verb2, string adverb)
     {
         Libs newLibs = new Libs();
         newLibs.Noun1 = noun1;
@@ -24,5 +26,28 @@ namespace MadLibs.Controllers
         return View(newLibs);
     }
 
+    [Route("DelphiaForm")]
+    public ActionResult DelphiaForm() { return View(); }
+
+
+    [Route("/Delphia")]
+    public ActionResult Delphia(string noun1, string noun2, string noun3, string noun4, string noun5, string noun6, string noun7, string noun8, string adjective1, string adjective2, string adjective3, string pastverb,string ingverb)
+    {
+        Libs newLibs = new Libs();
+        newLibs.Noun1 = noun1;
+        newLibs.Noun2 = noun2;
+        newLibs.Noun3 = noun3;
+        newLibs.Noun4 = noun4;
+        newLibs.Noun5 = noun5;
+        newLibs.Noun6 = noun6;
+        newLibs.Noun7 = noun7;
+        newLibs.Noun8 = noun8;
+        newLibs.Adjective1 = adjective1;
+        newLibs.Adjective2 = adjective2;
+        newLibs.Adjective3 = adjective3;
+        newLibs.PastVerb = pastverb;
+        newLibs.IngVerb = ingverb;
+        return View(newLibs);
+    }
   }
 }
--- MadLibs/Models/Libs.cs
+++ MadLibs/Models/Libs.cs
@@ -4,11 +4,22 @@ namespace MadLibs.Models
   {
     public string Noun1 { get; set; }
     public string Noun2 { get; set; }
+    public string Noun3 { get; set; }
+    public string Noun4 { get; set; }
+    public string Noun5 { get; set; }
+    public string Noun6 { get; set; }
+    public string Noun7 { get; set; }
+    public string Noun8 { get; set; }
     public string Adjective1 { get; set; }
     public string Adjective2 { get; set; }
+    public string Adjective3 { get; set; }
     public string Verb1 { get; set; }
     public string Verb2 { get; set; }
+    public string PastVerb { get; set; }
+    public string IngVerb { get; set; }
     public string Adverb { get; set; }
+
+
 
   }
 }
